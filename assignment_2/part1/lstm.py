c22b48d5a3828cd946430077f8a42221f9044dc8
StephanGrzelkowski <stephan.grzelkowski@gmail.com>
2019-11-20 18:25:16 +0100

    started init and forward for lstm and rnn

--- assignment_2/part1/lstm.py
+++ assignment_2/part1/lstm.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 ################################################################################
 
@@ -28,7 +29,55 @@ class LSTM(nn.Module):
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, device='cpu'):
         super(LSTM, self).__init__()
         # Initialization here ...
+        #init cell state 
+        num_cell = num_hidden
+        self.cell = {}
+        self.cell.state = torch.zeros(num_hidden)
 
+        #hidden layer
+        self.hidden = {}
+        self.hidden.state = torch.zeros(num_hidden)
+        self.hidden.params = {}
+        self.hidden.params['w'] = torch.randn(num_classes, num_hidden)
+        self.hidden.params['b'] = torch.randn(num_classes) 
+        #modulation gate
+        self.modulation_gate = {}
+        self.modulation_gate.params["w_x"] = torch.randn(num_cell, input_dim)
+  		self.modulation_gate.params["w_h"] = torch.randn(num_cell, num_hidden)
+  		self.modulation_gate.params['b'] = torch.randn(num_cell)
+
+  		#input gate
+        self.input_gate = {}
+        self.input_gate.params["w_x"] = torch.randn(num_cell, input_dim)
+  		self.input_gate.params["w_h"] = torch.randn(num_cell, num_hidden)
+  		self.input_gate.params['b'] = torch.randn(num_cell)
+  		
+  		#forget gate
+        self.forget_gate = {}
+        self.forget_gate.params["w_x"] = torch.randn(num_cell, input_dim)
+  		self.forget_gate.params["w_h"] = torch.randn(num_cell, num_hidden)
+  		self.forget_gate.params['b'] = torch.randn(num_cell)
+  		
+  		#output gate 
+  		self.output_gate = {}
+        self.output_gate.params["w_x"] = torch.randn(num_cell, input_dim)
+  		self.output_gate.params["w_h"] = torch.randn(num_cell, num_hidden)
+  		self.output_gate.params['b'] = torch.randn(num_cell)
+  		
     def forward(self, x):
-        # Implementation here ...
-        pass
+
+        # calculate all the gates
+        g = F.tanh(self.modulation_gate.params['w_x'] * x + self.modulation_gate.params['w_h'] * self.hidden.state + self.modulation_gate.params['b']) 
+        i = F.sigmoid(self.input_gate.params['w_x'] * x + self.input_gate.params['w_h'] * self.hidden.state, self.input_gate.params['b'])
+        f = F.sigmoid(self.forget_gate.params['w_x'] * x + self.forget_gate.params['w_h'] * self.hidden.state, self.forget_gate.params['b'])
+        o = F.sigmoid(self.output_gate.params['w_x'] * x + self.output_gate.params['w_h'] * self.hidden.state, self.output_gate.params['b'])
+        
+        #update cell state
+        self.cell.state = torch.matmul(g, i) + torch.matmul(self.cell.state, f)
+        
+        #update hidden state
+        self.hidden.state = torch.matmul(F.tanh(self.cell.state), o)
+
+        #get output
+        p = self.hidden.params['w'] * self.hidden.state + self.hidden.params['b']
+        out = F.softmax(p)
--- assignment_2/part1/vanilla_rnn.py
+++ assignment_2/part1/vanilla_rnn.py
@@ -28,7 +28,30 @@ class VanillaRNN(nn.Module):
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, device='cpu'):
         super(VanillaRNN, self).__init__()
         # Initialization here ...
+        #init weight matrix for input
+        self.hidden = {}
 
-    def forward(self, x):
+        self.hidden.params['w_in'] = torch.randn(num_hidden, input_dim, require_grad=True, device=device)
+        self.hidden.params['w_h'] = torch.randn(num_hidden, num_hidden, require_grad=True, device=device)
+        self.hidden.params['b_in'] = torch.randn(num_hidden, require_grad=True, device=device)
+
+        #init weight matrix for output
+        self.hidden.params['w_out'] =torch.randn(num_classes, num_hidden, require_grad=True, device=device)
+        self.hidden.params['b_out'] = torch.randn(num_hidden, require_grad=True, device=device)
+
+        #initialize a starting state
+       	self.hidden['state'] = torch.zeros(num_hidden, requres_grad=True, device=device)
+       	self.steps = seq_length
+
+     def forward(self, x):
         # Implementation here ...
-        pass
+        #update hidden state
+        input_hidden = self.hidden.params['w_h'] * x
+        hidden_hidden = self.hidden['state'] * self.hidden.params['w_h']
+        self.hidden['state'] = nn.functional.tanh( input_hidden + hidden_hidden + self.hidden.params['bias'])
+        
+        #calculate output
+        out = self.hidden['state'] * self.hidden.params['w_out']  + self.hidden.params['b_out']
+
+        return out
+
